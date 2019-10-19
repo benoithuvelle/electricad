a21e384aa306b6df75943443898bb5c575660cc8
Benoit Huvelle <benoithuvelle@mbp-de-benoit.home>
2019-10-19 21:25:55 +0200

    Updates

---
+++ precache-manifest.1d623d58c6a45fc640f3c5b0461247e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1ef97ccb8834e88fcb1998c80e3944",
+    "url": "/electricad/index.html"
+  },
+  {
+    "revision": "b63b5a40911a84523190",
+    "url": "/electricad/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8248fe50de06036f3bc1",
+    "url": "/electricad/static/js/2.b32d4dd2.chunk.js"
+  },
+  {
+    "revision": "b63b5a40911a84523190",
+    "url": "/electricad/static/js/main.b518d632.chunk.js"
+  },
+  {
+    "revision": "fea684205517bab5e1e8",
+    "url": "/electricad/static/js/runtime-main.2804d077.js"
+  }
+]);
